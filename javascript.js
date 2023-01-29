d16998cdf13053201c7cdc99721d77ca4a1a7150
joao-ar11 <jarlpereira29@gmail.com>
2023-01-29 16:11:27 -0300

    Change turn() to playRound() in javascript.js

--- javascript.js
+++ javascript.js
@@ -19,7 +19,7 @@ function getComputerChoice() {
     }
 }
 
-function turn(playerChoice, computerChoice) {
+function playRound(playerChoice, computerChoice) {
     if (playerChoice === computerChoice) {
         return "It's a tie!";
     } else if ((playerChoice === "rock" && computerChoice === "scissors") || (playerChoice === "paper" && computerChoice === "rock") || (playerChoice === "scissors" && computerChoice === "paper")) {
