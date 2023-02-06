9bcc6174ad6eabe3588ea2436c083f68ecbd6511
joao-ar11 <jarlpereira29@gmail.com>
2023-02-06 14:47:57 -0300

    Create score system

--- javascript.js
+++ javascript.js
@@ -16,16 +16,35 @@ function playRound(playerChoice, computerChoice) {
     if (playerChoice === computerChoice) {
         return "It's a tie!";
     } else if ((playerChoice === "rock" && computerChoice === "scissors") || (playerChoice === "paper" && computerChoice === "rock") || (playerChoice === "scissors" && computerChoice === "paper")) {
+        playerScore++;
         return `You won, ${playerChoice} beats ${computerChoice}!`;
     } else {
+        computerScore++;
         return `You lost, ${computerChoice} beats ${playerChoice}.`;
     }
 }
-
+let playerScore = 0;
+let computerScore = 0;
+const playerScoreDisplay = document.querySelector("#playerScore");
+playerScoreDisplay.textContent = playerScore;
+const computerScoreDisplay = document.querySelector("#computerScore");
+computerScoreDisplay.textContent = computerScore;
 const result = document.querySelector("#result");
 const rock = document.querySelector("#rock");
-rock.addEventListener("click", () => result.textContent = playRound("rock", getComputerChoice()));
+rock.addEventListener("click", () => {
+    result.textContent = playRound("rock", getComputerChoice());
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
+});
 const paper = document.querySelector("#paper");
-paper.addEventListener("click", () => result.textContent = playRound("paper", getComputerChoice()));
+paper.addEventListener("click", () => {
+    result.textContent = playRound("paper", getComputerChoice());
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
+});
 const scissors = document.querySelector("#scissors");
-scissors.addEventListener("click", () => result.textContent = playRound("scissors", getComputerChoice()));
+scissors.addEventListener("click", () => {
+    result.textContent = playRound("scissors", getComputerChoice());
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
+});
