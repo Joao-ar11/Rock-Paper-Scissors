1ded4a45eb2016236ae28a55c43b011f67605406
joao-ar11 <jarlpereira29@gmail.com>
2023-02-06 15:52:52 -0300

    Add checkForVictory()

--- javascript.js
+++ javascript.js
@@ -24,6 +24,18 @@ function playRound(playerChoice, computerChoice) {
     }
 }
 
+function checkForVictory() {
+    if (playerScore === 5) {
+        result.textContent = "You Won the Game!"
+        playerScore = 0;
+        computerScore = 0;
+    } else if (computerScore === 5) {
+        result.textContent = "You Lost the Game."
+        playerScore = 0;
+        computerScore = 0;
+    }
+}
+
 let playerScore = 0;
 let computerScore = 0;
 const playerScoreDisplay = document.querySelector("#playerScore");
@@ -36,16 +48,19 @@ rock.addEventListener("click", () => {
     result.textContent = playRound("rock", getComputerChoice());
     playerScoreDisplay.textContent = `You: ${playerScore}`;
     computerScoreDisplay.textContent = `Computer: ${computerScore}`;
+    checkForVictory();
 });
 const paper = document.querySelector("#paper");
 paper.addEventListener("click", () => {
     result.textContent = playRound("paper", getComputerChoice());
     playerScoreDisplay.textContent = `You: ${playerScore}`;
     computerScoreDisplay.textContent = `Computer: ${computerScore}`;
+    checkForVictory();
 });
 const scissors = document.querySelector("#scissors");
 scissors.addEventListener("click", () => {
     result.textContent = playRound("scissors", getComputerChoice());
     playerScoreDisplay.textContent = `You: ${playerScore}`;
     computerScoreDisplay.textContent = `Computer: ${computerScore}`;
+    checkForVictory();
 });
